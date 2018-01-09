45fb6c234324398fe3dbb6f682b8120c28f87977
Paulo Gladson Ximenes Pinheiro <paulo.pinheiro@serpro.gov.br>
2018-01-09 11:45:02 -0200

    Inicio

--- frontend/app/scripts/controllers/foto.js
+++ frontend/app/scripts/controllers/foto.js
@@ -21,39 +21,23 @@ app.controller('FotoController', [
     AlertService,
     FotoService
   ) {
-    $scope.uploadFiles = function(file, errFiles) {
-      $scope.f = file;
-      $scope.errFile = errFiles && errFiles[0];
-      if (file) {
-        file.upload = Upload.upload({
-          url: 'api/v1/fotos/upload',
-          data: {
-            file: file,
-            foto: { id: '1', description: 'teste' },
-            nomeArquivo: file.name,
-            tipoArquivo: file.type,
-            tamanhoArquivo: file.size
-          }
-        });
+    $scope.uploadPic = function(file) {
+      file.upload = Upload.upload({
+        url: 'https://angular-file-upload-cors-srv.appspot.com/upload',
+        data: {username: $scope.username, file: file},
+      });
 
-        file.upload.then(
-          function(response) {
-            $timeout(function() {
-              file.result = response.data;
-            });
-          },
-          function(response) {
-            if (response.status > 0)
-              $scope.errorMsg = response.status + ': ' + response.data;
-          },
-          function(evt) {
-            file.progress = Math.min(
-              100,
-              parseInt(100.0 * evt.loaded / evt.total)
-            );
-          }
-        );
+      file.upload.then(function (response) {
+        $timeout(function () {
+          file.result = response.data;
+        });
+      }, function (response) {
+        if (response.status > 0)
+          $scope.errorMsg = response.status + ': ' + response.data;
+      }, function (evt) {
+        // Math.min is to fix IE which reports 200% sometimes
+        file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+      });
       }
-    };
-  }
+    }
 ]);
